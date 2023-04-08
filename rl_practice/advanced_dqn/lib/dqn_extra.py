706252885e6a5a17dd45ada973c5df993e7d4f09
abbymark <abbymark@naver.com>
2023-04-08 13:05:03 +0900

    [ADD] distr_projection

--- rl_practice/advanced_dqn/lib/dqn_extra.py
+++ rl_practice/advanced_dqn/lib/dqn_extra.py
@@ -205,3 +205,45 @@ class DuelingDQN(nn.Module):
         adv = self.fc_adv(conv_out)
         val = self.fc_val(conv_out)
         return adv, val
+
+
+def distr_projection(next_distr, rewards, dones, gamma):
+    batch_size = len(rewards)
+    proj_distr = np.zeros((batch_size, N_ATOMS), dtype=np.float32)
+    delta_z = (Vmax - Vmin) / (N_ATOMS - 1)
+
+    for atom in range(N_ATOMS):
+        v = rewards + (Vmin + atom + delta_z) * gamma
+        tz_j = np.minimum(Vmax, np.maximum(Vmin, v))
+        b_j = (tz_j - Vmin) / delta_z
+        l = np.floor(b_j).astype(np.int64)
+        u = np.ceil(b_j).astype(np.int64)
+        eq_mask = u == l
+        proj_distr[eq_mask, l[eq_mask]] += \
+            next_distr[eq_mask, atom]
+        ne_mask = u != l
+        proj_distr[ne_mask, l[ne_mask]] += \
+            next_distr[ne_mask, atom] * (u - b_j)[ne_mask]
+        proj_distr[ne_mask, u[ne_mask]] += \
+            next_distr[ne_mask, atom] * (b_j - l)[ne_mask]
+    if dones.any():
+        proj_distr[dones] = 0.0
+        tz_j = np.minimum(
+            Vmax, np.maximum(Vmin, rewards[dones]))
+        b_j = (tz_j - Vmin) / delta_z
+        l = np.floor(b_j).astype(np.int64)
+        u = np.ceil(b_j).astype(np.int64)
+        eq_mask = u == l
+        eq_dones = dones.copy()
+        eq_dones[dones] = eq_mask
+        if eq_dones.any():
+            proj_distr[eq_dones, l[eq_mask]] = 1.0
+        ne_mask = u != l
+        ne_dones = dones.copy()
+        ne_dones[dones] = ne_mask
+        if ne_dones.any():
+            proj_distr[ne_dones, l[ne_mask]] = \
+                (u - b_j)[ne_mask]
+            proj_distr[ne_dones, u[ne_mask]] = \
+                (b_j - l)[ne_mask]
+    return proj_distr
